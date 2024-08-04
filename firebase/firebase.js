cc64c6e00424a68feba51016841f6328f3600059
crocmons <ilmasalsabil1006@gmail.com>
2024-08-05 03:21:27 +0600

    removed analytics

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import {getAuth} from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 
@@ -15,6 +15,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
+// export const analytics = getAnalytics(app);
 export const auth = getAuth(app)
 export const db = getFirestore(app);
